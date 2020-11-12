651fe5f4fe40fe3e1e8701eb616f8a9c7a8b0497
pragya-korpal <72054236+pragya-korpal@users.noreply.github.com>
2020-11-12 09:10:11 +0530

    2 modelAttribute

--- FlashJsp/src/main/java/com/example/demo/CustomerController.java
+++ FlashJsp/src/main/java/com/example/demo/CustomerController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,21 +14,29 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CustomerController {
+		
 	
-	
-	
-	
-	  @GetMapping("/") public String showForm(@ModelAttribute Customer customer){
-	  return "add_customer"; }
+	  @GetMapping("/") 
+	  public String showForm(@ModelAttribute Customer customer,@ModelAttribute Order order)
+	  {
+		  return "add_customer";
+	  }
 	 
-	
-	
 	  
+		/*
+		 * @PostMapping("/saveCustomer") public String saveCustomer(Customer
+		 * customer,RedirectAttributes redirectAttributes) {
+		 * redirectAttributes.addFlashAttribute("addedCustomer",customer); return
+		 * "redirect:/success";
+		 * 
+		 * }
+		 */
 	  
-	  
-	  @PostMapping("/saveCustomer") public String saveCustomer(Customer customer,RedirectAttributes redirectAttributes)
+	  @PostMapping("/saveForm") 
+	  public String saveForm(Customer customer,Order order,RedirectAttributes redirectAttributes)
 	  {
 		  redirectAttributes.addFlashAttribute("addedCustomer",customer); 
+		  redirectAttributes.addFlashAttribute("addedOrder",order); 
 		  return "redirect:/success";
 		  
 	  }
@@ -36,14 +46,21 @@ public class CustomerController {
 		  return "show_customer"; 
 	  }
 	  
-	  @RequestMapping(value = "/add-user", method = RequestMethod.POST)
-		public @ResponseBody Customer postEmployeeData(@RequestBody Customer customer) {
-	 
-			System.out.println("customer name  "+customer.getFirstName());
-			System.out.println("customer country  "+customer.getAddress().getCountry());
-			return customer;
-		}
-	 
-	
+		
+	  @RequestMapping(value = "/add-customer", method = RequestMethod.POST)
+	  public @ResponseBody Customer postCustomerData(@RequestBody Customer customer) 
+	  {
+		  	  System.out.println("customer name  "+customer.getFirstName());
+			  System.out.println("customer country  "+customer.getAddress().getCountry());
+			  return customer; 
+	  }
+	  
+	  @RequestMapping(value = "/add-order", method = RequestMethod.POST)
+	  public @ResponseBody Order postOrderData(@RequestBody Order order) 
+	  {
+		  	  System.out.println("product name  " + order.getProductName());
+			
+			  return order; 
+	  }
 
 }
--- FlashJsp/src/main/webapp/pages/add_customer.jsp
+++ FlashJsp/src/main/webapp/pages/add_customer.jsp
@@ -3,11 +3,12 @@
 <html>
 <head>
     <title>Title</title>
+    <script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
 </head>
 <body>
 
 
-<form:form action="saveCustomer" method="post" modelAttribute="customer">
+<%-- <form:form action="saveCustomer" method="post" modelAttribute="customer">
     <p>First Name: <form:input path="firstName" /></p>
     <p>Last Name: <form:input path="lastName" /></p>
     <p>Country: <form:input path="address.country" /></p>
@@ -15,10 +16,26 @@
     <p>Pin Code: <form:input path="address.pinCode" /></p>
  
      <input type="submit" value="Add Customer">
+</form:form> --%>
+
+
+<form:form id="CustomerForm" method="post" action="saveForm" modelAttribute="customer">
+    <p>First Name: <form:input path="firstName" /></p>
+    <p>Last Name: <form:input path="lastName" /></p>
+    <p>Country: <form:input path="address.country" /></p>
+    <p>City: <form:input path="address.city" /></p>
+    <p>Pin Code: <form:input path="address.pinCode" /></p>
+ 
+ 		<form:form id="OrderForm" method="post" action="saveForm" modelAttribute="order">
+		    <p>Product Name: <form:input path="productName" /></p>
+		    <p>Quantity: <form:input path="quantity" /></p>		
+		</form:form>
+     
+     <input type="button" value="Add Customer" onclick="submitForm()"/>
 </form:form>
 
  
- <%-- <form action="addCustomer" method="post">
+<%--  <form action="addCustomer" method="post">
 
 	<spring:bind path="customer.firstName">
 	   <input type="text"><br />
@@ -38,6 +55,14 @@
 	
 	 <input type="submit" value="Add Customer">
 </form> --%>
+
+<script type="text/javascript">
+	function submitForm(){
+	   $('#CustomerForm').submit();
+	   $('#OrderForm').submit();
+	}
+</script>
+
 
 </body>
 </html>
