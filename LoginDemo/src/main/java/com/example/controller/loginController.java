e3c846197ea52ccb55dafc186036c5f5ca69437e
ak-agarwal <55729950+ak-agarwal@users.noreply.github.com>
2020-11-17 14:57:55 +0530

    Update loginController.java

--- LoginDemo/src/main/java/com/example/controller/loginController.java
+++ LoginDemo/src/main/java/com/example/controller/loginController.java
@@ -2,7 +2,6 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,13 +22,19 @@ public class loginController {
 	@RequestMapping("/loginUser")
 	ModelAndView check(@RequestBody UserTab user) {
 		System.out.println("Username  " + user.getUsername());
+		System.out.println(user.getPassword());
 		ModelAndView mv = new ModelAndView();
-		if (user == userRepositroy.findByUsername(userRepositroy.findByUsername(user.getUsername()).getPassword())) {
-
+		String enteredPassword = user.getPassword();
+		String enteredUsername = user.getUsername();
+		UserTab sysUser = userRepositroy.findByUsername(enteredUsername);
+		System.out.println(sysUser.getUsername()+"  "+ sysUser.getPassword());
+		if (enteredPassword.equals( sysUser.getPassword())) {
 			mv.setViewName("success");
+			System.out.println("success");
 			return mv;
 		} else {
 			mv.setViewName("fail");
+			System.out.println("fail");
 			return mv;
 		}
 	}
