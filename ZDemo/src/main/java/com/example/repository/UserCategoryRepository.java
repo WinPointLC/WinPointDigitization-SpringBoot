615fe1a043675c9963b8772cfa1878018b4819c3
dixitabhishek239 <dixitabhishek239@icloud.com>
2020-12-07 19:20:11 +0530

    Repositories are created

---
+++ ZDemo/src/main/java/com/example/repository/UserCategoryRepository.java
@@ -0,0 +1,14 @@
+package com.example.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.model.Streams;
+import com.example.model.UserCategory;
+
+@Repository
+public interface UserCategoryRepository extends JpaRepository<UserCategory, Integer> {
+
+	
+	
+}
