6c98240670c4afda31312ba5acd86e861a8c04aa
dixitabhishek239 <dixitabhishek239@icloud.com>
2020-12-28 15:03:30 +0530

    add new user

--- WinPointManagementSystem/src/main/java/com/winpoint/controller/AddNewUserController.java
+++ WinPointManagementSystem/src/main/java/com/winpoint/controller/AddNewUserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.winpoint.model.EnquiryDetails;
 import com.winpoint.model.UserProfile;
+import com.winpoint.repository.UserProfileRepository;
 
 @Controller
 public class AddNewUserController {
@@ -27,43 +28,43 @@ public class AddNewUserController {
 		return mv;
 	}
 	
-//	@Autowired
-//	UserProfi
-//	
-//	@PostMapping("/saveNewUser")
-//	public String saveForm(UserProfile userProfile,RedirectAttributes redirectAttributes) throws ParseException {
-//		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-//		System.out.println("USER:  " + userProfile.getFirstName());
-//		System.out.println("USER:  " + userProfile.getGender());
-//		System.out.println("USER:  " + userProfile.getDegree());
-//		System.out.println("USER:  " + userProfile.getBirthDate());
-//		System.out.println("USER:  " + userProfile.getMappingTimeSlots());
-//		System.out.println("USER:  " + userProfile.getMappingSegmentType());		
-//		
+	@Autowired
+	UserProfileRepository userProfileRepository;
+	
+	@PostMapping("/saveNewUser")
+	public void saveForm(UserProfile userProfile,RedirectAttributes redirectAttributes) throws ParseException {
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("USER:  " + userProfile.getFirstName());
+		System.out.println("USER:  " + userProfile.getGender());
+		System.out.println("USER:  " + userProfile.getDegree());
+		System.out.println("USER:  " + userProfile.getBirthDate());
+		System.out.println("USER:  " + userProfile.getMappingTimeSlots());
+		System.out.println("USER:  " + userProfile.getMappingSegmentType());		
+		
 //		DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
 //		String t = userProfile.getBirthDate().toString();
 //		Date d = df.parse(t);
 //		userProfile.setBirthDate(d);
+
+//		String t1 = userProfile.getDateOfEnquiryString().toString();
+//		Date d1 = df.parse(t1);
+//		userProfile.setDateOfEnquiry(d1);
 //
-////		String t1 = userProfile.getDateOfEnquiryString().toString();
-////		Date d1 = df.parse(t1);
-////		userProfile.setDateOfEnquiry(d1);
-////
-////		String t2 = userProfile.getStartDateString().toString();
-////		Date d2 = df.parse(t2);
-////		userProfile.setStartDate(d2);
-//	
-//		
-////		enquiryDetailsRepository.save(enquiryDetails);
-////		finalUser=enquiryDetails;
-////		System.out.println("USER final:  " + finalUser.getBirthDateString());
-////		if(Location.equals("LoginForm"))
-////			return "LoginForm";
-////		else
-////			return "redirect:/EnquiryDetails";
-////		//return Location;
-//
-//	}
+//		String t2 = userProfile.getStartDateString().toString();
+//		Date d2 = df.parse(t2);
+//		userProfile.setStartDate(d2);
+	
+		
+		userProfileRepository.save(userProfile);
+//		finalUser=userProfile;
+//		System.out.println("USER final:  " + finalUser.getBirthDateString());
+//		if(Location.equals("LoginForm"))
+//			return "LoginForm";
+//		else
+//			return "redirect:/EnquiryDetails";
+//		return Location;
+
+	}
 
 
 }
