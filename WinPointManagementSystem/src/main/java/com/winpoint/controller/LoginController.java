8f68a39bd4a83694212e405a966579a48b77caeb
ak-agarwal <55729950+ak-agarwal@users.noreply.github.com>
2020-12-31 15:24:40 +0530

    Username

--- WinPointManagementSystem/src/main/java/com/winpoint/controller/LoginController.java
+++ WinPointManagementSystem/src/main/java/com/winpoint/controller/LoginController.java
@@ -1,34 +1,53 @@
 package com.winpoint.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.winpoint.model.Course;
-import com.winpoint.model.EnquiryDetails;
-import com.winpoint.model.SegmentType;
-import com.winpoint.model.TimeSlots;
-
 import com.winpoint.model.UserProfile;
-import com.winpoint.repository.CourseRepository;
-import com.winpoint.repository.SegmentTypeRepository;
-import com.winpoint.repository.TimeSlotsRepository;
+import com.winpoint.repository.UserProfileRepository;
+
 
 
 @Controller
 public class LoginController {
+	
+	@Autowired
+	UserProfileRepository userProfileRepositroy;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView LoginPage() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("LoginForm");	
 		return mv;
+	}
+	
+	
+	@RequestMapping(value = "loginUser", method = RequestMethod.POST)
+	public @ResponseBody String check(@RequestParam String username,@RequestParam String password) {
+		JSONObject data = new JSONObject();
+		UserProfile userObject = userProfileRepositroy.findByEmailId(username).get(0);
+		if (password.equals(userObject.getPassword())) {
+			if (userObject.getMappingUserCategory().getUserCategoryId() == 1) {
+				data.put("message", "success");
+				data.put("location","clientDash");
+				data.put("user", username);
+			} else {
+				data.put("message", "success");
+				data.put("location","employeeDash");
+				data.put("user", username);
+			}
+
+		} else {
+			data.put("message", "fail");
+		}
+		String dataString = data.toString();
+		return dataString;
 	}
 	
 
--- WinPointManagementSystem/src/main/java/com/winpoint/repository/UserProfileRepository.java
+++ WinPointManagementSystem/src/main/java/com/winpoint/repository/UserProfileRepository.java
@@ -1,10 +1,13 @@
 package com.winpoint.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.winpoint.model.Course;
 import com.winpoint.model.UserProfile;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Integer>{
-
+	
+	List<UserProfile> findByEmailId(String emailId);
+	
 }
