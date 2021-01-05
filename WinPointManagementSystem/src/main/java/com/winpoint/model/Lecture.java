3ad1bdd0841ffc2fe69d10efa9e8988c44dd050c
ak-agarwal <55729950+ak-agarwal@users.noreply.github.com>
2021-01-05 10:56:44 +0530

    Update Lecture.java

--- WinPointManagementSystem/src/main/java/com/winpoint/model/Lecture.java
+++ WinPointManagementSystem/src/main/java/com/winpoint/model/Lecture.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
@@ -45,4 +47,11 @@ public class Lecture {
 	@ManyToMany(mappedBy = "mappingLectures")
 	private List<CoursePlans> mappingCoursePlans = new ArrayList<CoursePlans>();
 
+	
+	@JsonIgnore
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinTable(name = "absenteesTable", joinColumns = @JoinColumn(name = "lectureId"), inverseJoinColumns = @JoinColumn(name = "userId"))
+	private List<UserProfile> mappingAbsentUsersList = new ArrayList<UserProfile>();
+	
+	
 }
